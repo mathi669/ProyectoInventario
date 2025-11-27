347b41b849f3eb493f2c4f38cd1c4099697f227f
mathi669 <matias.luarte@hotmail.com>
2025-11-27 13:54:28 -0300

    delete dup projects

--- inventario.backend/Models/DataContext.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace inventario.backend.Models
-{
-    public class DataContext
-    {
-    }
-}
--- inventario.front/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace inventario.front.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
