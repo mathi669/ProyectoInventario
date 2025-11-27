d15b22175e1e25a20857c1caa04bb5a10f5c553d
mathi669 <matias.luarte@hotmail.com>
2025-11-27 13:53:10 -0300

    add swagger

--- Inventario.backendV2/Program.cs
+++ Inventario.backendV2/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ConexionSQL")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -16,6 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
